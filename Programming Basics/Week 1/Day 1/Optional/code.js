9c0059ad230b42bc9b2ee09801c973e6b46727e2
1TAD0Ri <fehrialaddin@gmail.com>
2023-10-16 17:24:52 +0100

    day 1 c p o

---
+++ Programming Basics/Week 1/Day 1/Optional/code.js
@@ -0,0 +1,7 @@
+// this variable with number type store a data or a information 
+var likeCount = 0;
+//  the fyunction of this code will add 1 to var like count 
+function increaseLikes(){
+//  the fyunction of this code will add 1 to var like count 
+    likeCount = likeCount + 1;
+}
