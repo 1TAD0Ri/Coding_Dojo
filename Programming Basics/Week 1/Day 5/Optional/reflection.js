72e9e3120ac680a46db696e2f78879f99342712f
1TAD0Ri <fehrialaddin@gmail.com>
2023-10-23 12:17:46 +0100

    commit

---
+++ Programming Basics/Week 1/Day 5/Optional/reflection.js
@@ -0,0 +1,12 @@
+// // How have you noticed your bootcamp knowledge grow or change during Programming Basics?
+// it ' s still growing till this day with practice and searching in websites and i m feeling step by step i can grow more but 
+// to try surpasss my limits so much that's will not be good 
+
+
+// How have you noticed your approach to learning change throughout Programming Basics?
+//the pain in my brain that's it hhh
+
+
+
+// What do you consider your "Mistake of the Week," which was a failure you learned from during Programming Basics?
+// my mistake is not repating what i learned that day 
