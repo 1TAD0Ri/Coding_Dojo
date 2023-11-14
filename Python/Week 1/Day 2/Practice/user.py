c551462be6d9ebbac91777cf49fe6ab8c2c3b138
1TAD0Ri <fehrialaddin@gmail.com>
2023-11-14 20:07:11 +0100

    commit update user to redoit

--- Python/Week 1/Day 2/Practice/user.py
+++ Python/Week 1/Day 2/Practice/user.py
@@ -1,36 +1,79 @@
+# Assignment: User
+# For this assignment you will create the user class and add a couple methods!
+
+####### Note: NOT ALL CODE FROM THE VIDEO IS PROVIDED #######
+
 class User:
-    def __init__(self, first_name, last_name, email, age, is_rewards_member=False, gold_card_points=0):
+
+    def __init__(self, first_name, last_name, email, age):
+
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.age = age
-        self.is_rewards_member = is_rewards_member
-        self.gold_card_points = gold_card_points
+        self.is_rewards_member = False
+        self.gold_card_points = 0
+
+    # Methods:
+    # display_info(self) - Have this method print all 
+    # of the users' details on separate lines.
 
     def display_info(self):
-        print(f"Name: {self.first_name} {self.last_name}")
+        print("==========================")
+        print(f"First name: {self.first_name}")
+        print(f"Last name: {self.last_name}")
         print(f"Email: {self.email}")
         print(f"Age: {self.age}")
-        print(f"Rewards Member: {self.is_rewards_member}")
-        print(f"Gold Card Points: {self.gold_card_points}")
+        print(f"Member: {self.is_rewards_member}")
+        print(f"Current Points: {self.gold_card_points}")
+        print("==========================")
 
+    def enroll(self):
+
+        # NINJA BONUS
+        # Add logic in the enroll method to check if 
+        # they are a member already, and if they are, 
+        # print "User already a member." and return False, otherwise return True.
+
+        # REGULAR STUFF
+
+        # Have this method change the user's member
+        # status to True and 
+        self.is_rewards_member = True
+
+        # set their gold card points to 200.
+        #######   You can do it!
+
+
+    
+    
+    def spend_points(self, amount):
+
+        # Add logic in the spend points method
+        # to be sure they have enough points to 
+        # spend that amount and handle appropriately.
         
-def enroll(self):
-            self.its_rewards_member = True
-            self.gold_card_points = 200
-            print(f"enrolled in the rewards program , gold card set to 200.")
+        # PSEUDO CODE FOR NINJA BONUS
+        # if they don't have enough points:
+            # print to the console -- "You don't have enough points."
+            # then return # exit function!
+
+        # decrease the user's points by the amount specified.
+        self.gold_card_points
+        # .... this line is incomplete .. how do set this to a new value?
+
+    
+
+    # Ninja Bonuses:
 
 
 
-#     def spend_points(self, amount):
-#         if self.is_rewards_member:
-#             if self.gold_card_points >= amount:
-#                 self.gold_card_points -= amount
-#                 print(f"Spent {amount} gold card points. Remaining points: {self.gold_card_points}")
-#             else:
-#                 print("Insufficient gold card points.")
-#         else:
-#             print("Must be a rewards member to spend gold card points.")
+my_user = User("Sadie", "Flick", "sflick@codingdojo.com", 99)
+my_user.display_info()
+my_user.enroll()
+my_user.display_info()
+my_user.spend_points(100)
+my_user.display_info()
 
 
 
