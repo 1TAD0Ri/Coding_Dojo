8511f122f6a104771498f7f54f12d98a7b9853e8
1TAD0Ri <fehrialaddin@gmail.com>
2023-11-16 20:16:14 +0100

    commit

--- Python/Week 1/Day 3/Practice/basketballdictionnaire.py
+++ Python/Week 1/Day 3/Practice/basketballdictionnaire.py
@@ -1,23 +1,22 @@
 # Player class with updated constructor
 class Player:
     def __init__(self, data):
-        self.name = data['name']
-        self.age = data['age']
-        self.position = data['position']
-        self.team = data['team']
-    
+        self.name = data["name"]
+        self.age = data["age"]
+        self.position = data["position"]
+        self.team = data["team"]
 
     # * NINJA BONUS class mehotd
     @classmethod
-    def add_players(cls, data):
+    def add_players(cls, data): 
         player_objects = []
         for dict in data:
             player_objects.append(cls(dict))
         return player_objects
-    
+
     # Not required for the assignment but useful
-    # __repr__(self) is a python system method that 
-    # tells python how to handle representing that class 
+    # __repr__(self) is a python system method that
+    # tells python how to handle representing that class
     # when, for example the object is printed to the terminal.
     def __repr__(self):
         display = f"Player: {self.name}, {self.age} y/o, pos: {self.position}, team: {self.team}"
@@ -25,23 +24,24 @@ class Player:
 
 
 kevin = {
-        "name": "Kevin Durant", 
-        "age":34, 
-        "position": "small forward", 
-        "team": "Brooklyn Nets"
+    "name": "Kevin Durant",
+    "age": 34,
+    "position": "small forward",
+    "team": "Brooklyn Nets",
 }
 jason = {
-        "name": "Jason Tatum", 
-        "age":24, 
-        "position": "small forward", 
-        "team": "Boston Celtics"
+    "name": "Jason Tatum",
+    "age": 24,
+    "position": "small forward",
+    "team": "Boston Celtics",
 }
 kyrie = {
-        "name": "Kyrie Irving", 
-        "age":32, "position": "Point Guard", 
-        "team": "Brooklyn Nets"
+    "name": "Kyrie Irving",
+    "age": 32,
+    "position": "Point Guard",
+    "team": "Brooklyn Nets",
 }
-    
+
 # Create your Player instances here!
 player_jason = Player(jason)
 player_kevin = Player(kevin)
