1c407d71267879205f78b62501d37c7c4bd98abd
1TAD0Ri <fehrialaddin@gmail.com>
2023-12-18 13:50:13 +0100

    commit

---
+++ MERNStack/week_1/Day_1/CPO/core/predict_destructing.js
@@ -0,0 +1,18 @@
+/* 
+problem 1 = Tesla
+            mercedes
+
+problem 2 = name is not defined
+Elon
+
+problem 3 = 12345
+undefined
+
+problem 4 =false 
+true 
+
+problem 5 ='value
+instrument.ts:129 (6) [1, 5, 1, 8, 3, 3]
+1
+5
+undefined'
---
+++ MERNStack/week_1/Day_1/CPO/practice/hoist.js
@@ -0,0 +1,20 @@
+/*for code 1 console.log(hello); logs undefined
+
+for code 2 code undefined 
+
+for code 3 'super cool'
+
+for code 4 'chhiken' / 'half-chiken '
+
+for code 5 'mean is not a function'
+
+for code 6 'undefined
+            rock
+            r&b'
+
+for code 7 'san jose
+            seattle
+            burbank
+            san jose'
+
+for code 8 'type error '
