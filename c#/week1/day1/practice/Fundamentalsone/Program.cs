c439f409325c35e59f469b0656e90351eb79acbc
1TAD0Ri <fehrialaddin@gmail.com>
2024-01-23 18:24:03 +0100

    commit

---
+++ c#/week1/day1/practice/Fundamentalsone/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// for(int i = 1; i <= 255; i++)
+// {
+//     Console.WriteLine(i);
+// }
+
+
+// Random rand = new Random();
+// int sum = 0;
+
+// for (int i = 1; i <= 5; i++)
+// {
+//     int randomValue = rand.Next(10, 21);
+//     Console.WriteLine(randomValue);
+//     sum += randomValue;
+// }
+
+// Console.WriteLine("Sum of random values: " + sum);
+
+
+// for (int i = 1; i <= 100; i++)
+// {    
+//     if (i % 3 == 0 && i % 5 == 0)
+//     {
+//         Console.WriteLine("FizzBuzz");
+//     }
+//     else if (i % 3 == 0)
+//     {
+//         Console.WriteLine("Fizz");
+//     }
+//     else if (i % 5 == 0)
+//     {
+//         Console.WriteLine("Buzz");
+//     }
+//     else
+//     {
+//         Console.WriteLine(i);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
